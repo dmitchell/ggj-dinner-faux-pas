9c896a0d8a54226919a0540631b34d9f452ee0fe
Chris Hurd <churd82@gmail.com>
2016-01-30 18:33:22 -0500

    Update player_ops.js
    not compilable

--- src/js/player_ops.js
+++ src/js/player_ops.js
@@ -8,60 +8,61 @@ var PLAYER_OPS = {
 	    }
 	    
 	},
-	{course: "main",
-	utensil: "knife",
-	effect: {
-		gertrude: [0.2, 0.3]
+	{
+	    course: "main",
+	    utensil: "knife",
+	    effect: {gertrude: [0.2, 0.3]
+	    }
 	},
-	{course: "main",
-	utensil: "spoon",
-	effect: {
-		gertrude: [0, -0.2]
+	{
+	    course: "main",
+	    utensil: "spoon",
+	    effect: {gertrude: [0, -0.2]}
 	},
-	{course: "main",
-	utensil: null,
-	effect: {
-		gertrude: [-0.2, 0]
+	{
+	    course: "main",
+	    utensil: null,
+	    effect: {gertrude: [-0.2, 0]}
 	},
-	{course: "first",
-	utensil: "fork",
-	effect: {
-		gertrude: [0, -0.3]
+	{
+	    course: "first",
+	    utensil: "fork",
+	    effect: {gertrude: [0, -0.3]}
 	},
-	{course: "first",
-	utensil: "knife",
-	effect: {
-		gertrude: [0, -0.2]
+	{
+	    course: "first",
+	    utensil: "knife",
+	    effect: {gertrude: [0, -0.2]}
 	},
-	{course: "first",
-	utensil: "spoon",
-	effect: {
-		gertrude: [0.3, 0.3]
+	{
+	    course: "first",
+	    utensil: "spoon",
+	    effect: {gertrude: [0.3, 0.3]}
 	},
-	{course: "first",
-	utensil: null,
-	effect: {
-		gertrude: [-0.4, 0]
+	{
+	    course: "first",
+	    utensil: null,
+	    effect: {gertrude: [-0.4, 0]}
 	},
-	{course: "second",
-	utensil: "fork",
-	effect: {
-		gertrude: [0.3, 0.3]
+	{
+	    course: "second",
+	    utensil: "fork",
+	    effect: {gertrude: [0.3, 0.3]}
 	},
-	{course: "second",
-	utensil: "knife",
-	effect: {
-		gertrude: [0, -0.2]
+	{
+	    course: "second",
+	    utensil: "knife",
+	    effect: {gertrude: [0, -0.2]}
 	},
-	{course: "second",
-	utensil: "spoon",
-	effect: {
-		gertrude: [0, -0.2]
+	{
+	    course: "second",
+	    utensil: "spoon",
+	    effect: {gertrude: [0, -0.2]}
 	},
-	{course: "second",
-	utensil: null,
-	effect: {
-		gertrude: [-0.3, 0]
+	{
+	    course: "second",
+	    utensil: null,
+	    effect: {gertrude: [-0.3, 0]}
 	},
 	],
 	fork: [{
@@ -81,48 +82,75 @@ var PLAYER_OPS = {
 	}]
     },
     speech: {
-    	respond_ent_hamlet: {speaker: "hamlet",
-	    topic: "food",
-	    emoticon: "excited",
-	    effect: {
-		hamlet: [0, 0.1]
-	    },	
-    	},
-	respond_food_hamlet: {
-	    speaker: "hamlet",
-	    topic: "food",
-	    emoticon: "excited",
-	    effect: {
-		hamlet: [0, 0.1]
-	    },
-	    {
-	    speaker: "hamlet",
-	    topic: "food",
-	    emoticon: "confused",
-	    effect: {
-		hamlet: [0.1, 0]
-	    },
-	    {
-	    speaker: "hamlet",
-	    topic: "food",
-	    emoticon: "neutral",
-	    effect: {
-		hamlet: [-0.1, 0]
-	    },
-	    {
-	    speaker: "hamlet",
-	    topic: "food",
-	    emoticon: "suspicious",
-	    effect: {
-		hamlet: [0.1, 0]
-	    },
-	    {
-	    speaker: "hamlet",
-	    topic: "food",
-	    emoticon: "angry",
-	    effect: {
-		hamlet: [0, -0.1]
-	    }
+    	hamlet: {
+	      ent: { 
+	          excited: {
+        	    hamlet: [0.1, 0]
+	          },
+	          confused: {
+	              hamlet: [-0.1, 0]
+	          },
+	          neutral: {
+	              hamlet: [0, 0.1]
+	          },
+	          suspicious: {
+	              hamlet: [-0.1, 0]
+	          },
+	          angry: {
+	              hamlet: [0, -0.1]
+	          }
+	      }
+	      family: {
+	            excited: {
+	                hamlet: [-0.1, 0]
+	            },
+	            confused: {
+	                hamlet: [0, 0.1]
+	            },
+	            neutral: {
+	                hamlet: [0, -0.1]
+	            },
+	            suspicious: {
+	                hamlet: [0, 0.1]
+	            },
+	            angry: {
+	                hamlet: [0.1, 0]
+	            }
+	        }
+	        food: {
+	            excited: {
+	                hamlet: [0, 0.1]
+	            },
+	            confused: {
+	                hamlet: [0.1, 0]
+	            },
+	            neutral: {
+	                hamlet: [-0.1, 0]
+	            },
+	            suspicious: {
+	                hamlet: [0.1, 0]
+	            },
+	            angry: {
+	                hamlet: [0, -0.1]
+	            }
+	        }
+	        gov {
+	            excited: {
+	                hamlet: [-0.1, 0]
+	            },
+	            confused: {
+	                hamlet: [0.1, 0]
+	            },
+	            neutral: {
+	                hamlet: [-0.1, 0]
+	            },
+	            suspicious: {
+	                hamlet: [0.1, 0]
+	            },
+	            angry: {
+	                hamlet: [0, -0.1]
+	            }
+	        }
 	    
 	}
     }
