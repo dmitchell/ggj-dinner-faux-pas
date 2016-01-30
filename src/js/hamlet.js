3b31dca0f6596d7ac211bfaffc595121b99fc555
Chris Hurd <churd82@gmail.com>
2016-01-30 14:09:50 -0500

    Update hamlet.js

--- src/js/hamlet.js
+++ src/js/hamlet.js
@@ -22,9 +22,7 @@ var GAME_SETUP =
 	    	p: 1/60,
 	    	target: "claudius",
 	    	effect: {claudius: [-0.02, 0]}
-	    }
-	    }
-	    }
+	    },
 	    sword: {
 		happy: [0, 0.05], // end game
 		p: 0.5, // almost certain to occur in 5 seconds (1 - 0.5^5)
@@ -80,7 +78,7 @@ var GAME_SETUP =
 		clarity: [0, 0.39],
 		p: 1/60, // once per minute
 		target: "all", // or all or player
-		effect: {hamlet: [-0.04, 0], ophelia: [-0.05, 0], gertrude: [-0.02, 0]} // this model has the weakness
+		effect: {hamlet: [-0.04, 0], ophelia: [-0.02, 0], gertrude: [-0.05, 0]} // this model has the weakness
 	     // that claudius's and gertrude's responses should be a function of 
              // their current state: if confused, then they may ignore the interaction.
              // if they are happy, then they should get confused not angry
@@ -92,9 +90,7 @@ var GAME_SETUP =
 	    	p: 1/60,
 	    	target: "hamlet",
 	    	effect: {hamlet: [-0.02, 0]}
-	    }
-	    }
-	    }
+	    },
 	    sword: {
 		happy: [0, 0.05], // end game
 		p: 0.5, // almost certain to occur in 5 seconds (1 - 0.5^5)
@@ -118,28 +114,163 @@ var GAME_SETUP =
   {
     happy: [0, .39],
     clarity: [0, .39],
-    image: "hamlet_angry.jpg"
+    image: "claudius_angry.jpg"
   }
   {
   	happy: [.7, 1],
   	clarity: [0, .39],
-  	image: "hamlet_confused.jpg"
+  	image: "claudius_confused.jpg"
   }
   {
   	happy: [0, .39],
   	clarity: [.7, 1],
-  	image: "hamlet_suspicious.jpg"
+  	image: "claudius_suspicious.jpg"
   }
   {
   	happy: [.7, 1],
   	clarity: [.7, 1],
-  	image: "hamlet_excited.jpg"
+  	image: "claudius_excited.jpg"
   }
   {
-  	image: "hamlet_neutral.jpg"
+  	image: "claudius_neutral.jpg"
   }
   ]
  },
+ ophelia: 
+   {happy: .9, // starting value scale 0..1
+    clarity: .6,
+    operations: {
+	anytime: {
+	    dagger_all: {
+		happy: [0, 0.39], // if happiness between these
+		clarity: [0, 0.39],
+		p: 1/60, // once per minute
+		target: "all", // or all or player
+		effect: {claudius: [-0.04, 0], gertrude: [-0.05, 0], hamlet: [-0.02, 0]} // this model has the weakness
+	     // that claudius's and gertrude's responses should be a function of 
+             // their current state: if confused, then they may ignore the interaction.
+             // if they are happy, then they should get confused not angry
+             // if they are angry, it should amplify
+	    },
+	    dagger_gertrude: {
+	    	happy: [0, 0.39],
+	    	clarity: [0.7, 1],
+	    	p: 1/60,
+	    	target: "gertrude",
+	    	effect: {gertrude: [-0.02, 0]}
+	    },
+	    sword: {
+		happy: [0, 0.05], // end game
+		p: 0.5, // almost certain to occur in 5 seconds (1 - 0.5^5)
+		target: "ophelia",
+		endgame: "ophelia" // ends the game with ophelia dead
+	    }
+	},
+	drinks: {
+	    // shows how some operators can have different effects during different courses
+	    toast: {
+		happy: [.6, 1.0],
+		clarity: [0, 1.0],  // anytime
+		p: 1/(60 * 3), // once every 3 mins
+		target: "all",
+		effect: {ophelia: [0.05, 0], claudius: [0.1, 0], gertrude: [0.05, 0], hamlet: [0.1, 0]}
+	    }
+	}
+    },
+  appearance: [
+      // which image to use for each happiness x clarity range
+  {
+    happy: [0, .39],
+    clarity: [0, .39],
+    image: "ophelia_angry.jpg"
+  },
+  {
+  	happy: [.7, 1],
+  	clarity: [0, .39],
+  	image: "ophelia_confused.jpg"
+  },
+  {
+  	happy: [0, .39],
+  	clarity: [.7, 1],
+  	image: "ophelia_suspicious.jpg"
+  },
+  {
+  	happy: [.7, 1],
+  	clarity: [.7, 1],
+  	image: "ophelia_excited.jpg"
+  },
+  {
+  	image: "ophelia_neutral.jpg"
+  }
+  ]
+ },
+ gertrude: 
+   {happy: .6, // starting value scale 0..1
+    clarity: .9,
+    operations: {
+	anytime: {
+	    dagger_all: {
+		happy: [0, 0.39], // if happiness between these
+		clarity: [0, 0.39],
+		p: 1/60, // once per minute
+		target: "all", // or all or player
+		effect: {hamlet: [-0.02, 0], ophelia: [-0.04, 0], claudius: [-0.05, 0]} // this model has the weakness
+	     // that claudius's and gertrude's responses should be a function of 
+             // their current state: if confused, then they may ignore the interaction.
+             // if they are happy, then they should get confused not angry
+             // if they are angry, it should amplify
+	    },
+	    dagger_ophelia: {
+	    	happy: [0, 0.39],
+	    	clarity: [0.7, 1],
+	    	p: 1/60,
+	    	target: "ophelia",
+	    	effect: {ophelia: [-0.02, 0]}
+	    },
+	    sword: {
+		happy: [0, 0.05], // end game
+		p: 0.5, // almost certain to occur in 5 seconds (1 - 0.5^5)
+		target: "player",
+		endgame: "player" // ends the game with claudius dead
+	    }
+	},
+	drinks: {
+	    // shows how some operators can have different effects during different courses
+	    toast: {
+		happy: [.6, 1.0],
+		clarity: [0, 1.0],  // anytime
+		p: 1/(60 * 3), // once every 3 mins
+		target: "all",
+		effect: {ophelia: [0.05, 0], claudius: [0.1, 0], gertrude: [0.05, 0], hamlet: [0.1, 0]}
+	    }
+	}
+    },
+  appearance: [
+      // which image to use for each happiness x clarity range
+  {
+    happy: [0, .39],
+    clarity: [0, .39],
+    image: "gertrude_angry.jpg"
+  },
+  {
+  	happy: [.7, 1],
+  	clarity: [0, .39],
+  	image: "gertrude_confused.jpg"
+  },
+  {
+  	happy: [0, .39],
+  	clarity: [.7, 1],
+  	image: "gertrude_suspicious.jpg"
+  },
+  {
+  	happy: [.7, 1],
+  	clarity: [.7, 1],
+  	image: "gertrude_excited.jpg"
+  },
+  {
+  	image: "gertrude_neutral.jpg"
+  }
+  ]
  },
  course: {
      drinks: {
@@ -158,7 +289,6 @@ var GAME_SETUP =
      }
      successful: {
      	start: 240
-     }
      }
    }
 }
