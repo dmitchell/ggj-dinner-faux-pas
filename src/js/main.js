475231fd2def7fa7b8fd7f92c7c0e1324488ca00
kirkjerk <kirkjerk@gmail.com>
2016-01-29 23:49:26 -0500

    whoops here is the timer stuff

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,23 @@
-function setup(){
-    var canvas = createCanvas(500, 500);
-    canvas.parent("canvasParent");
-};
-function draw(){
-    stroke(random(255),128);
-    line(random(500),random(500),mouseX,mouseY);
+
+var GLOBALS = {
+    baseinterval:1000
+}
+
+
+$(document).ready(function(){
+    gogogo(); 
+});
+
+
+function gogogo(){
+    if(Math.random() < .2){
+        listLog("tick: HIT!");    
+    } else {
+        listLog("tick");    
+    }
+    setTimeout(gogogo,GLOBALS.baseinterval);
+}
+
+function listLog(msg){
+    $("#msglist").append("<li>"+msg+"</li>");
 }
