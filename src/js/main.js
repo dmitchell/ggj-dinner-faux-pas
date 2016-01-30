f6aa84c68b1dad646ec42ce7081ebfa08707b5d8
kirkjerk <kirkjerk@gmail.com>
2016-01-30 10:51:10 -0500

    blah blah

--- src/js/main.js
+++ src/js/main.js
@@ -10,19 +10,12 @@ $(document).ready(function(){
     // TODO initialize course
     // TODO initialize npcs
     // TODO initialize player
-    gogogo(); 
 });
 
 
 function gogogo(){
     // TODO check course expiration
     // TODO move each player
-    if(Math.random() < .2){
-        listLog("tick: HIT!");    
-    } else {
-        listLog("tick");    
-    }
-    setTimeout(gogogo,GLOBALS.baseinterval);
 }
 
 function listLog(msg){
