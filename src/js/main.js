ebc15af18ba168aa7ac4b94042d78c9add3b8e28
Don Mitchell <dmitchell@placester.com>
2016-01-30 10:38:15 -0500

    Basic move operator

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,22 @@
-
 var GLOBALS = {
-    baseinterval:1000
+    baseinterval:1000,
+    npcs: {},
+    course: null,
+    time: 0,
 }
 
 
 $(document).ready(function(){
+    // TODO initialize course
+    // TODO initialize npcs
+    // TODO initialize player
     gogogo(); 
 });
 
 
 function gogogo(){
+    // TODO check course expiration
+    // TODO move each player
     if(Math.random() < .2){
         listLog("tick: HIT!");    
     } else {
--- src/js/npc.js
+++ src/js/npc.js
@@ -1,14 +1,38 @@
-var Npc = function(name, descriptor) {
+var NPC = function(name, descriptor) {
     this.name = name;
     this.happy = descriptor.happy || .6;
-    this.gets_it = descriptor.gets_it || .6;
+    this.clarity = descriptor.clarity || .6;
+    // TODO normalize probabilities
     this.operations = descriptor.operations;
+    this.topic = null;
+    GLOBALS.npcs[name] = this;
+    // TODO draw npc
 }
 
-Npc.prototype.move = function() {
+// figures out what if any moves this npc makes in this cycle
+// at most one move per cycle per NPC
+NPC.prototype.move = function() {
+    // roll the dice against all available moves
+    for (var phase in [GLOBALS.course, "anytime"]) {
+	var phaseOps = this.operations[phase];
+	for (var idx = 0; idx < phaseOps.length; idx++) {
+	    var op = phaseOps[idx];
+	    if (Math.random() < op) {
+		// TODO invoke graphics
+		// affect each target
+		for (var target in op.effect) {
+		    GLOBALS.npcs[target].react(op.effect[target]);
+		}
+	    }
+	}
+    } 
 }
 
-Npc.prototype.react = function(effect) {
-    this.happy += effect.claudius[0];  // should this be *=?
-    this.gets_it += effect.claudius[1];
+NPC.prototype.react = function(effect) {
+    this.happy += effect[this.name][0];  // should this be *=?
+    this.clarity += effect[this.name][1];
+    if (effect[this.name].length > 2) {
+	this.topic = effect[this.name][2];
+    }
+    // TODO invoke character image change
 }
