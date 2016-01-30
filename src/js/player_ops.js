f0e76df87d7da00dc95b00b113a74a301d0c00aa
Chris Hurd <churd82@gmail.com>
2016-01-30 17:42:51 -0500

    Update player_ops.js

--- src/js/player_ops.js
+++ src/js/player_ops.js
@@ -6,7 +6,27 @@ var PLAYER_OPS = {
 	    effect: {
 		gertrude: [0.3, 0.2]
 	    }
-	}],
+	    
+	},
+	{course: "main",
+	utensil: "knife",
+	effect: {
+		gertrude: [0.2, 0.3]
+	},
+	{course: "main",
+	utensil: "spoon",
+	effect: {
+		gertrude: [0, -0.2]
+	},
+	{course: "main",
+	utensil: null,
+	effect: {
+		gertrude: [-0.2, 0]
+	},
+	{course: "first",
+	}
+	
+	],
 	fork: [{
 	    effect: {
 		utensil: "fork"
@@ -27,10 +47,39 @@ var PLAYER_OPS = {
 	respond_food_hamlet: {
 	    speaker: "hamlet",
 	    topic: "food",
-	    emoticon: "cry",
+	    emoticon: "excited",
 	    effect: {
-		hamlet: [-0.1, 0], claudius: [0.04, 0]
+		hamlet: [-0.1, 0]
+	    },
+	    {
+	    speaker: "hamlet",
+	    topic: "food",
+	    emoticon: "confused",
+	    effect: {
+		hamlet: [-0.1, 0]
+	    },
+	    {
+	    speaker: "hamlet",
+	    topic: "food",
+	    emoticon: "neutral",
+	    effect: {
+		hamlet: [-0.1, 0]
+	    },
+	    {
+	    speaker: "hamlet",
+	    topic: "food",
+	    emoticon: "suspicious",
+	    effect: {
+		hamlet: [-0.1, 0]
+	    },
+	    {
+	    speaker: "hamlet",
+	    topic: "food",
+	    emoticon: "angry",
+	    effect: {
+		hamlet: [-0.1, 0]
 	    }
+	    
 	}
     }
 }
